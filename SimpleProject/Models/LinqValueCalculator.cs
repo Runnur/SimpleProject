fa4b9dd88020b2ec928cab78424d473a50e81b32
Denis Yurchenko <yurdenis@gmail.com>
2018-12-29 10:41:38 +0300

    New files

---
+++ SimpleProject/Models/LinqValueCalculator.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleProject.Models
+{
+    public class LinqValueCalculator
+    {
+        public decimal ValueProducts(IEnumerable<Product> products)
+        {
+            return products.Sum(p => p.Price);
+        }
+    }
+}
