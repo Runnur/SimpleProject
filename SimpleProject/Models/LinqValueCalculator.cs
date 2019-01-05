534a072c6956d33a3a6a85f266b6cf9f8c837240
Denis Yurchenko <yurdenis@gmail.com>
2019-01-05 12:08:55 +0300

    Add files

---
+++ SimpleProject/Models/LinqValueCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleProject.Models
+{
+    public class LinqValueCalculator
+    {
+        public decimal ValueProducts(IEnumerable<Product> products)
+        {
+            return products.Sum(p => p.Price);
+        }
+        public LinqValueCalculator()
+        {
+        }
+    }
+}
---
+++ SimpleProject/Models/ShoppingCart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleProject.Models
+{
+    public class ShoppingCart
+    {
+        private LinqValueCalculator calc;
+
+        public ShoppingCart(LinqValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+
+        public IEnumerable<Product> Products { get; set; }
+
+        public decimal CalculateProductTotal()
+        {
+            return calc.ValueProducts(Products);
+        }
+        public ShoppingCart()
+        {
+        }
+    }
+}
