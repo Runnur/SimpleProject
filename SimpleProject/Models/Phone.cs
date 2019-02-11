f2e9c7f45da968eea3688ab7b4e5a636f226571e
Denis Yurchenko <yurdenis@gmail.com>
2019-02-11 11:36:41 +0300

    Description

--- SimpleProject/Models/Phone.cs
+++ SimpleProject/Models/Phone.cs
@@ -6,6 +6,7 @@ namespace SimpleProject.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Company { get; set; }
+        public string Description { get; set; }
         public Phone()
         {
         }
